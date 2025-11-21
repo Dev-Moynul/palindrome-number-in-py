550144153bda9e74995a89aaef01e85490b97b63
Dev-Moynul <stu.moynulislam@gmail.com>
2025-11-21 13:06:19 +0600

    palindrome number in py

--- palindrome_number.py
+++ palindrome_number.py
@@ -2,10 +2,8 @@ def is_palindrome(num):
     s = str(num)
     return s == s[::-1]
 
-# take input
 n = int(input("Enter a number: "))
 
-# call function
 if is_palindrome(n):
     print(n, "is a palindrome number")
 else:
